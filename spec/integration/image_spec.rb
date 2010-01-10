971b900fce0656ad47487cbbc2b0f9632e4235a4
Willem van Bergen <willem@vanbergen.org>
2010-01-10 08:59:26 +0100

    Added a working ChunkyPNG::Image spec.

--- spec/integration/image_spec.rb
+++ spec/integration/image_spec.rb
@@ -2,17 +2,21 @@ require File.expand_path('../spec_helper.rb', File.dirname(__FILE__))
 
 describe ChunkyPNG::Image do
 
-  it "should write a valid PNG image" do
+  it "should write a valid PNG image using an indexed palette" do
     image    = ChunkyPNG::Image.new(10, 20, ChunkyPNG::Color.rgb(10, 100, 255))
     filename = resource_file('testing.png')
     File.open(filename, 'w') { |f| image.write(f) }
-    
-    
+
     png = ChunkyPNG.load(filename)
     png.header_chunk.width.should  == 10
     png.header_chunk.height.should == 20
+    png.header_chunk.color.should  == ChunkyPNG::Chunk::Header::COLOR_INDEXED
+
+    png.palette_chunk.should_not be_nil
     png.data_chunks.should_not be_empty
-    `open #{filename}`
+    # `open #{filename}`
+    
+    File.unlink(filename)
   end
 end
 
