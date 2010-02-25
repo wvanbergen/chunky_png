f4dbb92d8053a2f95b42ad8f414160580d8d033e
Willem van Bergen <willem@vanbergen.org>
2010-02-24 22:35:51 -0500

    Make RMagick specs not raise errors if RMagick is not installed.

--- spec/chunky_png/rmagick_spec.rb
+++ spec/chunky_png/rmagick_spec.rb
@@ -1,19 +1,23 @@
 require 'spec_helper'
 
-require 'chunky_png/rmagick'
+begin
+  require 'chunky_png/rmagick'
 
-describe ChunkyPNG::RMagick do
+  describe ChunkyPNG::RMagick do
   
-  it "should import an image from RMagick correctly" do
-    image = Magick::Image.read(resource_file('16x16_non_interlaced.png')).first
-    canvas = ChunkyPNG::RMagick.import(image)
-    canvas.should == reference_canvas('16x16_non_interlaced')
-  end
+    it "should import an image from RMagick correctly" do
+      image = Magick::Image.read(resource_file('16x16_non_interlaced.png')).first
+      canvas = ChunkyPNG::RMagick.import(image)
+      canvas.should == reference_canvas('16x16_non_interlaced')
+    end
   
-  it "should export an image to RMagick correctly" do
-    canvas = reference_canvas('16x16_non_interlaced')
-    image  = ChunkyPNG::RMagick.export(canvas)
-    image.format = 'PNG32'
-    canvas.should == ChunkyPNG::Canvas.from_blob(image.to_blob)
+    it "should export an image to RMagick correctly" do
+      canvas = reference_canvas('16x16_non_interlaced')
+      image  = ChunkyPNG::RMagick.export(canvas)
+      image.format = 'PNG32'
+      canvas.should == ChunkyPNG::Canvas.from_blob(image.to_blob)
+    end
   end
+rescue LoadError => e
+  # skipping RMagick tests
 end
