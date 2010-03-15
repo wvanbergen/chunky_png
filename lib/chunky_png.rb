4a0d6872e6edea5c7f41cb5dc2db55c49cf6acc7
Willem van Bergen <willem@vanbergen.org>
2010-03-15 06:33:07 -0400

    Released chunky_png gem version 0.7.0

--- lib/chunky_png.rb
+++ lib/chunky_png.rb
@@ -27,7 +27,7 @@ module ChunkyPNG
 
   # The current version of ChunkyPNG. This value will be updated automatically
   # by them gem:release rake task.
-  VERSION = "0.6.0"
+  VERSION = "0.7.0"
 
   ###################################################
   # PNG international standard defined constants
