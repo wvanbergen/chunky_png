176830101ec02d54064093dcd38aca0e84fe06db
Willem van Bergen <willem@vanbergen.org>
2010-09-18 20:21:10 +0200

    Added some more decoding benchmarks.

--- benchmarks/decoding_benchmark.rb
+++ benchmarks/decoding_benchmark.rb
@@ -16,9 +16,11 @@ def image_data(name)
   data
 end
 
-png_stream       = image_data('pixelstream_reference.png')
-rgba_pixelstream = image_data('pixelstream.rgba')
-rgb_pixelstream  = image_data('pixelstream.rgb')
+no_filtering_stream    = image_data('pixelstream_fast_rgba.png')
+up_filtering_stream    = image_data('pixelstream_reference.png')
+paeth_filtering_stream = image_data('pixelstream_best_compression.png')
+rgba_pixelstream       = image_data('pixelstream.rgba')
+rgb_pixelstream        = image_data('pixelstream.rgb')
 
 n = (ENV['N'] || '5').to_i
 
@@ -28,7 +30,9 @@ puts "---------------------------------------------"
 puts
 
 Benchmark.bmbm do |x|
-  x.report('From PNG stream')       { n.times { ChunkyPNG::Image.from_blob(png_stream) } }
+  x.report('PNG - no filtering')    { n.times { ChunkyPNG::Image.from_blob(no_filtering_stream) } }
+  x.report('PNG - UP filtering')    { n.times { ChunkyPNG::Image.from_blob(up_filtering_stream) } }
+  x.report('PNG - PAETH filtering') { n.times { ChunkyPNG::Image.from_blob(paeth_filtering_stream) } }
   x.report('From RGBA pixelstream') { n.times { ChunkyPNG::Image.from_rgba_stream(240, 180, rgba_pixelstream) } }
   x.report('From RGB pixelstream')  { n.times { ChunkyPNG::Image.from_rgb_stream(240, 180, rgb_pixelstream) } }
 end
