a8480c7868a3edcc6ddc32b71123e5bf175688e1
Willem van Bergen <willem@vanbergen.org>
2010-09-19 07:15:12 +0200

    Improved filesize benchmark.

--- benchmarks/filesize_benchmark.rb
+++ benchmarks/filesize_benchmark.rb
@@ -6,11 +6,25 @@ Bundler.setup
 require 'benchmark'
 require 'chunky_png'
 
-filename = File.join(File.dirname(__FILE__), '..', 'spec', 'resources', 'pixelstream_reference.png')
-image = ChunkyPNG::Canvas.from_file(filename)
+files = ['pixelstream_reference.png', 'operations.png', 'gray_10x10.png', 'clock_stubbed.png']
 
-puts ":no_compression   : %d byte" % image.to_blob(:no_compression).bytesize
-puts ":fast_rgba        : %d byte" % image.to_blob(:fast_rgba).bytesize
-puts ":fast_rgb         : %d byte" % image.to_blob(:fast_rgb).bytesize
-puts ":good_compression : %d byte" % image.to_blob(:good_compression).bytesize
-puts ":best_compression : %d byte" % image.to_blob(:best_compression).bytesize
+def encode_png(image, constraints = {})
+  filesize = nil
+  time = Benchmark.realtime { filesize = image.to_blob(constraints).bytesize }
+  [filesize, time]
+end
+
+files.each do |file|
+  filename = File.join(File.dirname(__FILE__), '..', 'spec', 'resources', file)
+  image = ChunkyPNG::Canvas.from_file(filename)
+
+  puts "#{file}: #{image.width}x#{image.height} - #{image.palette.size} colors"
+  puts "------------------------------------------------"
+  puts "<default>         : %8d bytes in %0.4fs" % encode_png(image)
+  puts ":no_compression   : %8d bytes in %0.4fs" % encode_png(image, :no_compression)
+  puts ":fast_rgba        : %8d bytes in %0.4fs" % encode_png(image, :fast_rgba)
+  puts ":fast_rgb         : %8d bytes in %0.4fs" % encode_png(image, :fast_rgb)
+  puts ":good_compression : %8d bytes in %0.4fs" % encode_png(image, :good_compression)
+  puts ":best_compression : %8d bytes in %0.4fs" % encode_png(image, :best_compression)
+  puts
+end
