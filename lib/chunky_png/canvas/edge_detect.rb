fa95bef3c4eaad79645440deee3d86d7e6c440c1
sausheong <sausheong@gmail.com>
2011-05-02 01:30:27 +0800

    converted the image to grayscale first before using it for edge detection

--- lib/chunky_png/canvas/edge_detect.rb
+++ lib/chunky_png/canvas/edge_detect.rb
@@ -23,16 +23,17 @@ module ChunkyPNG
       def edge_detect(algo=:sobel)      
         al_x, al_y = SOBEL_X, SOBEL_Y if algo == :sobel
         al_x, al_y = PREWITT_X, PREWITT_Y if algo == :prewitt
+        orig = self.grayscale # change the original image to grayscale first
         edge = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
         for x in 1..width-2
           for y in 1..height-2
-            pixel_x = (al_x[0][0] * at(x-1,y-1)) + (al_x[0][1] * at(x,y-1)) + (al_x[0][2] * at(x+1,y-1)) +
-                      (al_x[1][0] * at(x-1,y))   + (al_x[1][1] * at(x,y))   + (al_x[1][2] * at(x+1,y)) +
-                      (al_x[2][0] * at(x-1,y+1)) + (al_x[2][1] * at(x,y+1)) + (al_x[2][2] * at(x+1,y+1))
+            pixel_x = (al_x[0][0] * orig.at(x-1,y-1)) + (al_x[0][1] * orig.at(x,y-1)) + (al_x[0][2] * orig.at(x+1,y-1)) +
+                      (al_x[1][0] * orig.at(x-1,y))   + (al_x[1][1] * orig.at(x,y))   + (al_x[1][2] * orig.at(x+1,y)) +
+                      (al_x[2][0] * orig.at(x-1,y+1)) + (al_x[2][1] * orig.at(x,y+1)) + (al_x[2][2] * orig.at(x+1,y+1))
 
-            pixel_y = (al_y[0][0] * at(x-1,y-1)) + (al_y[0][1] * at(x,y-1)) + (al_y[0][2] * at(x+1,y-1)) +
-                      (al_y[1][0] * at(x-1,y))   + (al_y[1][1] * at(x,y))   + (al_y[1][2] * at(x+1,y)) +
-                      (al_y[2][0] * at(x-1,y+1)) + (al_y[2][1] * at(x,y+1)) + (al_y[2][2] * at(x+1,y+1))
+            pixel_y = (al_y[0][0] * orig.at(x-1,y-1)) + (al_y[0][1] * orig.at(x,y-1)) + (al_y[0][2] * orig.at(x+1,y-1)) +
+                      (al_y[1][0] * orig.at(x-1,y))   + (al_y[1][1] * orig.at(x,y))   + (al_y[1][2] * orig.at(x+1,y)) +
+                      (al_y[2][0] * orig.at(x-1,y+1)) + (al_y[2][1] * orig.at(x,y+1)) + (al_y[2][2] * orig.at(x+1,y+1))
 
             val = Math.sqrt((pixel_x * pixel_x) + (pixel_y * pixel_y)).ceil
             edge[x,y] = ChunkyPNG::Color.grayscale(val)
@@ -41,7 +42,6 @@ module ChunkyPNG
         edge
       end
 
-      private
       def at(x,y)
         ChunkyPNG::Color.to_grayscale_bytes(self[x,y]).first
       end    
