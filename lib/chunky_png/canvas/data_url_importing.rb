999bdc2d3b7b56547f37cf5ba6d928032a3b4b62
Willem van Bergen <willem@vanbergen.org>
2011-09-14 08:47:09 -0400

    RDoc fixes.

--- lib/chunky_png/canvas/data_url_importing.rb
+++ lib/chunky_png/canvas/data_url_importing.rb
@@ -1,12 +1,14 @@
 module ChunkyPNG
   class Canvas
 
-    # Methods to export a canvas to a PNG data URL.
+    # Methods to import a canvas from a PNG data URL.
     module DataUrlImporting
 
-      # Exports the canvas as a data url (e.g. data:image/png;base64,<data>) that can
-      # easily be used inline in CSS or HTML.
-      # @return [String] The canvas formatted as a data URL string.
+      # Imports a canvas from a PNG data URL.
+      # @param [String] string The data URL string to load from.
+      # @return [Canvas] The imported canvas.
+      # @raise ChunkyPNG::SignatureMismatch if the provides string is not a properly
+      #    formatted PNG data URL (i.e. it should start with "data:image/png;base64,")
       def from_data_url(string)
         if string =~ %r[^data:image/png;base64,((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=))$]
           from_blob($1.unpack('m').first)
