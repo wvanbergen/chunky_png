ea89023f7b03f88a3e67977f0bd9f6f0c706b4d7
Guy Wydouw <guy@wydouw.be>
2014-09-01 15:29:03 +0200

    fix extension warehouse

--- lib/chunky_png/compatibility.rb
+++ lib/chunky_png/compatibility.rb
@@ -1,10 +1,10 @@
 # Define the byte-operators on a string if they're not defined (Ruby 1.8)
 
-class String
-  alias_method :getbyte, :[]    unless method_defined?(:getbyte)
-  alias_method :setbyte, :[]=   unless method_defined?(:setbyte)
-  alias_method :bytesize, :size unless method_defined?(:bytesize)
-end
+# class String
+#   alias_method :getbyte, :[]    unless method_defined?(:getbyte)
+#   alias_method :setbyte, :[]=   unless method_defined?(:setbyte)
+#   alias_method :bytesize, :size unless method_defined?(:bytesize)
+# end
 #
 # module Enumerable
 #   unless method_defined?(:minmax)
